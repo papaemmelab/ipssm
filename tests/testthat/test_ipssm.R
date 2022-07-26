890e8c6c9fbb66acfbeb51ce9203a6d62febab07
Juanes <juanes.ao@gmail.com>
2022-07-26 14:31:24 -0400

    ✅ improve test job output

--- tests/testthat/test_ipssm.R
+++ tests/testthat/test_ipssm.R
@@ -19,25 +19,38 @@ test_that("ipssm examples are computed as expected", {
   dd.annot <- IPSSMannotate(dd.res)
 
   # 5) Check expected results against actual results
-  pp347 <- dd.annot[dd.annot$ID == "pp347", ]
+  expected_results = list(
+    pp347 = list(
+      IPSSMscore = 0.31,
+      IPSSMcat = "Moderate High",
+      IPSSMscore_mean = 0.31,
+      IPSSMcat_mean = "Moderate High",
+      IPSSMscore_best = 0.3,
+      IPSSMcat_best = "Moderate High",
+      IPSSMscore_worst = 0.63,
+      IPSSMcat_worst = "High"
+    ),
+    pp564 = list(
+      IPSSMscore = 4.55,
+      IPSSMcat = "Very High",
+      IPSSMscore_mean = 4.55,
+      IPSSMcat_mean = "Very High",
+      IPSSMscore_best = 4.55,
+      IPSSMcat_best = "Very High",
+      IPSSMscore_worst = 4.55,
+      IPSSMcat_worst = "Very High"
+    )
+  )
 
-  expect_equal(pp347$IPSSMscore, 0.31)
-  expect_equal(pp347$IPSSMcat, "Moderate High")
-  expect_equal(pp347$IPSSMscore_mean, 0.31)
-  expect_equal(pp347$IPSSMcat_mean, "Moderate High")
-  expect_equal(pp347$IPSSMscore_best, 0.3)
-  expect_equal(pp347$IPSSMcat_best, "Moderate High")
-  expect_equal(pp347$IPSSMscore_worst, 0.63)
-  expect_equal(pp347$IPSSMcat_worst, "High")
+  for (patient in names(expected_results)) {
+    for (field in names(expected_results[[patient]])) {
+      actual_result <- dd.annot[dd.annot$ID == patient, field]
+      expected_result <- expected_results[[patient]][[field]]
 
-  pp564 <- dd.annot[dd.annot$ID == "pp564", ]
-
-  expect_equal(pp564$IPSSMscore, 4.55)
-  expect_equal(pp564$IPSSMcat, "Very High")
-  expect_equal(pp564$IPSSMscore_mean, 4.55)
-  expect_equal(pp564$IPSSMcat_mean, "Very High")
-  expect_equal(pp564$IPSSMscore_best, 4.55)
-  expect_equal(pp564$IPSSMcat_best, "Very High")
-  expect_equal(pp564$IPSSMscore_worst, 4.55)
-  expect_equal(pp564$IPSSMcat_worst, "Very High")
+      cat(paste("Field: ", field, "\n"))
+      cat(paste("\t", expected_result, " (Expected)\n"))
+      cat(paste("\t", actual_result, " (Actual)\n"))
+      expect_equal(actual_result, expected_result)
+    }
+  }
 })
